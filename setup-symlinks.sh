ecebed0006257b87fa2d0c9b70d5a4cab5035703
Max Fierke <max@maxfierke.com>
2013-05-01 21:57:40 -0500

    Fix symlinks script

--- setup-symlinks.sh
+++ setup-symlinks.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # Places softlinks to dotfiles in user's home
-ln -s .bashrc $HOME/.bashrc
-ln -s .vimrc $HOME/.vimrc
-ln -s .vim $HOME/.vim
+ln -s `pwd`/.bashrc $HOME/.bashrc
+ln -s `pwd`/.vimrc $HOME/.vimrc
+ln -s `pwd`/.vim $HOME/.vim
 mkdir -p $HOME/.config/fish/
-ln -s config.fish $HOME/.config/fish/config.fish
+ln -s `pwd`/config.fish $HOME/.config/fish/config.fish
 
